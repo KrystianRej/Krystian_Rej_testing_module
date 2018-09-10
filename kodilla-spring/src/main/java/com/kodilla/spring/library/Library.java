1bc846fe7a2c26c1d669d4f2777e4a5bbe9f7795
KrystianRej <krystian.rej@gmail.com>
2018-09-10 18:20:00 +0200

    save

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -10,16 +10,15 @@ import java.util.List;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
-    @Autowired
+    //@Autowired
     private LibraryDbController libraryDbController;
 
-    /*@Autowired
     public Library(LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
     }
 
     public Library() {
-    }*/
+    }
     /*
     @Autowired
     public void setLibraryDbController(LibraryDbController libraryDbController){
